7a33e30818d290494bdcc111652637a1576e5919
Chau Vu <chv004@ucsd.edu>
2020-11-05 19:59:48 -0800

    Added input image capturing

---
+++ flutter-frontend/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'camera_screen.dart';
+
+CameraDescription rearCamera;
+
+Future<void> main()  async{
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+  rearCamera = cameras.first;
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MLPrep',
+      theme: ThemeData.dark(),
+      home: CameraScreen(camera: rearCamera)
+    );
+  }
+}
