bba33ba17c005a6f2547b7405591dbe32332c9f6
Emily Amspoker <eamspoker21@kentdenver.org>
2020-11-08 20:24:08 -0700

    add delete functionality, basic add functionality

--- flutter-frontend/lib/ingredients.dart
+++ flutter-frontend/lib/ingredients.dart
@@ -11,11 +11,11 @@ class IngredientsList extends StatefulWidget {
 }
 
 class _IngredientsListState extends State<IngredientsList> {
-
+  TextEditingController nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     List<Widget> children = new List<Widget>();
-    
+
     widget.ingredients.forEach((item) {
       children.add(
         new Row(
@@ -28,34 +28,77 @@ class _IngredientsListState extends State<IngredientsList> {
       );
     });
 
+    void addItemToList() {
+      setState(() {
+        widget.ingredients.insert(0, nameController.text);
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("List of Ingredients"),
         actions: <Widget>[
-        FlatButton(
-          textColor: Colors.white,
-          onPressed: () async {
-            var url = "http://127.0.0.1:5000/${widget.ingredients.join(',')}";
-            //var response = await http.get(url);
-          },
-          child: Text("Confirm"),
-          shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
-        ),
-      ],
-      ),
-      body: ListView(
-        padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 75.0),
-        children: children,
-      ),
-      floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 50.0),
-          child: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              
-            })
+          FlatButton(
+            textColor: Colors.white,
+            onPressed: () async {
+              var url = "http://127.0.0.1:5000/${widget.ingredients.join(',')}";
+              //var response = await http.get(url);
+            },
+            child: Text("Confirm"),
+            shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
           ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        ],
+      ),
+      body: Column(children: <Widget>[
+        Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: widget.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = widget.ingredients[index];
+                  return Dismissible(
+                    key: Key(item),
+                    direction: DismissDirection.startToEnd,
+                    child: ListTile(
+                      title: Text(item),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete_forever),
+                        onPressed: () {
+                          setState(() {
+                            widget.ingredients.removeAt(index);
+                          });
+                        },
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      setState(() {
+                        widget.ingredients.removeAt(index);
+                      });
+                    },
+                  );
+                })),
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
+            child: Row(children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: nameController,
+                    onSubmitted: (text) {},
+                  ),
+                ),
+              ),
+              FloatingActionButton(
+                  child: Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      FocusScope.of(context).unfocus();
+                      addItemToList();
+                    });
+                  })
+            ]))
+      ]),
     );
   }
 }
